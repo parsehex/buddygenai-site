4b4b4d6f4046a40c88bba7d336ae4bee0cae294f
parsehex <1779937+parsehex@users.noreply.github.com>
2024-04-16 19:30:13 -0400

    clean up, only show title

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,22 @@
 <template>
 	<div class="App">
 		<header class="App-header">
-			<img :src="logo" class="App-logo" alt="logo" />
-			<p>BuddyGen</p>
-			<p>
-				<button type="button" @click="incrementCount">
-					count is: {{ count }}
-				</button>
-			</p>
-			<p>Edit <code>App.vue</code> and save to test HMR updates.</p>
-			<p>
-				<a
-					class="App-link"
-					href="https://vuejs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn Vue
-				</a>
-				|
-				<a
-					class="App-link"
-					href="https://vitejs.dev/guide/features.html"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Vite Docs
-				</a>
-			</p>
+			<!-- <img :src="logo" class="App-logo" alt="logo" /> -->
+			<!-- <p>BuddyGen</p> -->
+			<!-- create a stylized name BuddyGenAI -->
+			<!-- BuddyGen should be blue, AI black or dark gray -->
+			<h1>
+				<span style="color: #61dafb">BuddyGen</span>
+				<span style="color: #111">AI</span>
+			</h1>
 		</header>
 	</div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
 // @ts-ignore
 import logo from './logo.svg';
 import './App.css';
-
-const count = ref(0);
-
-const incrementCount = () => {
-	count.value++;
-};
 </script>
 
 <style scoped>
