12d32b9309d9172364907f63ebe3f9f5763dde9b
parsehex <1779937+parsehex@users.noreply.github.com>
2024-04-16 20:54:53 -0400

    add logo, favicon

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
 	<div class="App">
 		<header class="App-header">
-			<!-- <img :src="logo" class="App-logo" alt="logo" /> -->
-			<!-- <p>BuddyGen</p> -->
-			<!-- create a stylized name BuddyGenAI -->
-			<!-- BuddyGen should be blue, AI black or dark gray -->
+			<img :src="logo" class="App-logo" alt="logo" />
 			<h1>
 				<span style="color: #61dafb">BuddyGen</span>
 				<span style="color: #111">AI</span>
@@ -15,7 +12,7 @@
 
 <script setup lang="ts">
 // @ts-ignore
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 </script>
 
