c9fac678191d5ecb36b54d5c022b33592a6a7240
parsehex <1779937+parsehex@users.noreply.github.com>
2024-05-06 09:43:09 -0400

    update
    - add setup instructions link
    - add cover pic

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,34 @@
 				<span style="color: #111">AI</span>
 			</h1>
 		</header>
+
+		<div class="App-body">
+			<div class="App-body-left">
+				<div class="App-body-left-title">
+					<a
+						href="https://github.com/parsehex/BuddyGenAI/blob/main/docs/how-to-setup.md"
+						target="_blank"
+						style="color: #dcdcdc; font-size: 1.5rem"
+					>
+						How to setup app
+					</a>
+				</div>
+			</div>
+		</div>
+		<br />
+		<img src="./cover.png" alt="cover" style="width: 75%" />
 	</div>
+	<p
+		style="
+			text-align: center;
+			padding: 10px;
+			background-color: #282c34;
+			color: #dcdcdc;
+			text-align: center;
+		"
+	>
+		Copyright © 2024 Thomas Mays
+	</p>
 </template>
 
 <script setup lang="ts">
